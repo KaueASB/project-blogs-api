3ca271ef505f092f319122ad31c922faa3c357b8
Kaue Alves <kaue_alves00@yahoo.com>
2022-07-14 15:07:14 -0300

    feat/ concluindo requisito 7

---
+++ src/database/models/category.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * @param {import('sequelize').Sequelize} DataTypes 
+ * @param {import('sequelize').Sequelize} sequelize 
+*/
+
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    name: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    tableName: 'Categories',
+    timestamps: false, 
+  });
+
+  return Category;
+};
